bc093b3adba2c18da9bff1f298ca0c0c8ed920d1
matt-rhys-jones <mattinahat@gmx.com>
2016-08-18 17:04:35 +0100

    [SETUP] add *basic* content indexing - requires cleanup

--- gulp-tasks/compile/nunjucks.js
+++ gulp-tasks/compile/nunjucks.js
@@ -1,21 +1,38 @@
 import paths from '../../config/paths';
 
+const path = require('path');
 const gulp = require('gulp');
 const nunjucks = require('gulp-nunjucks');
+const data = require('gulp-data');
+const frontMatter = require('gulp-front-matter');
 
-const config = {
+const nunjucksConfig = {
   paths: {
     css: '/css/styles.css'
-  }
+  },
+  content: [] // gets populated by compile:index task
 };
 
-const glob = [];
+gulp.task('compile:index',() => {
+  const glob = [];
+  glob.push(paths.content.root + '/**/*.md');
 
-glob.push(paths.app.layout + '/**/*.html');
-glob.push('!' + paths.app.layout + '/base.html');
+  return gulp.src(glob)
+    .pipe(frontMatter())
+    .pipe(data((file) => {
+      nunjucksConfig.content.push({
+        metadata: file.frontMatter,
+        path: '/content/' + path.basename(file.path, '.md') + '.html'
+      })
+    }))
+});
 
-gulp.task('compile:nunjucks', ['compile:content'], () =>
+gulp.task('compile:nunjucks', ['compile:content', 'compile:index'], () => {
+  const glob = [];
+  glob.push(paths.app.layout + '/**/*.html');
+  glob.push('!' + paths.app.layout + '/base.html');
+
   gulp.src(glob)
-    .pipe(nunjucks.compile(config))
+    .pipe(nunjucks.compile(nunjucksConfig))
     .pipe(gulp.dest('dist'))
-);
+});
