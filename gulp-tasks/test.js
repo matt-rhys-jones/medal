104d4b57fa9ef3c999f08b18120f5407cb326543
matt-rhys-jones <mattinahat@gmx.com>
2016-08-18 19:14:25 +0100

    [SETUP] add coverage reporting with babel istanbul

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -1,14 +1,17 @@
 const gulp = require('gulp');
 const Server = require('karma').Server;
 
-/**
- * Run test once and exit
- */
+// Set environment to test so Babel Istanbul plugin is loaded
+const env = process.env.NODE_ENV;
+process.env.NODE_ENV = 'test';
+
 gulp.task('test:js', function (done) {
+
   new Server({
     configFile: __dirname + '/../karma.conf.js',
     singleRun: true
   }, () => {
+    process.env.NODE_ENV = env;
     done() 
   }).start();
 });
@@ -18,6 +21,7 @@ gulp.task('test:js:tdd', function (done) {
     configFile: __dirname + '/../karma.conf.js',
     singleRun: false
   }, () => {
+    process.env.NODE_ENV = env;
     done()
   }).start();
 });
--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,24 @@
 module.exports = function(config) {
   config.set({
     preprocessors: {
-      'medal/**/*.js': ['babel']
+      'medal/js/modules/**/*.js': ['babel'],
+      'medal/js/tests/**/*.spec.js': ['babel']
+    },
+    babelPreprocessor: {
+      options: {
+          "presets": ["es2015"],
+          "plugins": ["transform-es2015-modules-umd"]
+      }
+    },
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      type : 'text'
     },
     browsers: ['PhantomJS'],
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'chai'],
     files: [
-      'medal/**/*.js'
+      'medal/js/modules/**/*.js',
+      'medal/js/tests/**/*.spec.js'
     ]
   });
 };
